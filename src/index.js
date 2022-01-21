a60ce03eee63cbce7cbdab5ec5e82d959e496ffc
yurykurylovich <yury.kurylovich@gmail.com>
2022-01-22 01:34:32 +0300

    Finished step 1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {program} from "commander";
+
+export default () => {
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .parse(process.argv)
+};
