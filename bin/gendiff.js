a41f0f8fd0cef32696ee11f9ef8b8e24f030ddb2
yurykurylovich <yury.kurylovich@gmail.com>
2022-01-22 21:31:53 +0300

    Add comparator function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@ import {program} from "commander";
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
-
+import comparator from "../src/comparator.js";
 
 program
   .version('0.0.1')
@@ -15,12 +15,12 @@ program.action((filepath1, filepath2) => {
   const file1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8')
   const file2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8')
 
-  console.log('file1', file1)
-  console.log('file2', file2)
+  const obj1 = JSON.parse(file1)
+  const obj2 = JSON.parse(file2)
+
+  comparator(obj1, obj2)
 
 })
-
-
 
 program
   .parse(process.argv)
---
+++ src/comparator.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+
+let obj1 = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}
+
+let obj2 = {
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+}
+
+const comparator = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+  const keys = [...keys1, ...keys2]
+
+  const cb = (acc, key) => {
+    if (_.isEqual(obj1[key], obj2[key])) {
+      acc[`  ${key}`] = obj1[key]
+    } else {
+      if (_.has(obj1, key)) {
+        acc[`- ${key}`] = obj1[key]
+      }
+      if (_.has(obj2, key)) {
+        acc[`+ ${key}`] = obj2[key]
+      }
+    }
+
+    return acc;
+  };
+
+  const resultObj = keys.reduce(cb, {})
+
+  const result = JSON.stringify(resultObj, null, '  ')
+    .split('"')
+    .join('')
+    .split(',')
+    .join('');
+
+  console.log(result)
+}
+
+export default comparator;
