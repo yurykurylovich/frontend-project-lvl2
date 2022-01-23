0982388c2c8ef635ebb7923246d6a0b4116d92e8
yurykurylovich <yury.kurylovich@gmail.com>
2022-01-23 15:21:59 +0300

    Another linter check

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
-import {program} from "commander";
+import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
-import comparator from "../src/comparator.js";
+import comparator from '../src/comparator.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>');
 
 program.action((filepath1, filepath2) => {
-  const file1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8')
-  const file2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8')
+  const file1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
+  const file2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
 
-  const obj1 = JSON.parse(file1)
-  const obj2 = JSON.parse(file2)
+  const obj1 = JSON.parse(file1);
+  const obj2 = JSON.parse(file2);
 
-  comparator(obj1, obj2)
-
-})
+  comparator(obj1, obj2);
+});
 
 program
-  .parse(process.argv)
+  .parse(process.argv);
